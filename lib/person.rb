6902bb147f3d05af340c067785970433b4ab48f0
lordish <gitonga403@gmail.com>
2023-05-23 09:02:46 +0300

    final commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,38 @@
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each do |key,value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+my_attr = {name:"Morris", color:"Chocolate", hobby:"Golf"}
+morris = Person.new(my_attr)
+puts morris.hobby
+
+class Car 
+  def initialize(attributes)
+    attributes.each do |key,value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=",value)
+    end
+  end
+end
+
+car_attr = {name:"BMW", year:2004, color:"black"}
+car = Car.new(car_attr)
+puts car.name
+
+class Dog
+  def initialize(attributes)
+    attributes.each do |key,value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=",value)
+    end
+  end
+end
+
+dog_attr = {name:"Nick", color:"Black", breed:"GSD"}
+dog = Dog.new(dog_attr)
+puts dog.color
